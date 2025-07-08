7b121cf567a9add5efcad126cc1e6a24e3eff059
JagerMidnight <therealjager@hotmail.com>
2025-07-08 14:23:35 -0500

    Create mythic-shop.js

---
+++ public/pages/shop/mythic-shop.js
@@ -0,0 +1,58 @@
+const mythicData = {
+    featured: [
+        {
+            name: "Cyber Demon Genji",
+            imageUrl: "https://overwatch.blizzard.com/assets/images/heroes/genji-large.f274a496.png", // Placeholder image
+            type: "Mythic Skin",
+            price: "80 Mythic Prisms",
+            description: "Unleash your inner demon with this highly customizable Genji skin."
+        },
+        {
+            name: "Zeus Junker Queen",
+            imageUrl: "https://overwatch.blizzard.com/assets/images/heroes/junker-queen-large.0a624838.png", // Placeholder image
+            type: "Mythic Skin",
+            price: "80 Mythic Prisms",
+            description: "Command the battlefield with the power of Zeus."
+        }
+    ],
+    availableSoon: [
+        {
+            name: "A-7000 Wargod Ana",
+            imageUrl: "https://overwatch.blizzard.com/assets/images/heroes/ana-large.a622557e.png", // Placeholder image
+            type: "Mythic Skin",
+            description: "A battle-hardened medic, upgraded for the future."
+        }
+    ],
+    previous: [
+        {
+            name: "Amaterasu Kiriko",
+            imageUrl: "https://overwatch.blizzard.com/assets/images/heroes/kiriko-large.e2d3d9b0.png", // Placeholder image
+            type: "Mythic Skin",
+            price: "80 Mythic Prisms",
+            description: "Embrace the divine spirit with Kiriko's Amaterasu form."
+        },
+        {
+            name: "Galactic Emperor Sigma",
+            imageUrl: "https://overwatch.blizzard.com/assets/images/heroes/sigma-large.9458c5a4.png", // Placeholder image
+            type: "Mythic Skin",
+            price: "80 Mythic Prisms",
+            description: "Rule the cosmos with this majestic Sigma skin."
+        },
+        {
+            name: "Adventurer Tracer",
+            imageUrl: "https://overwatch.blizzard.com/assets/images/heroes/tracer-large.92213e4b.png", // Placeholder image
+            type: "Mythic Skin",
+            price: "80 Mythic Prisms",
+            description: "Always ready for the next expedition, even through time."
+        }
+    ],
+    aspects: [
+        {
+            name: "Bound Demon Reinhardt Weapon",
+            imageUrl: "https://overwatch.blizzard.com/assets/images/heroes/reinhardt-large.55b1129b.png", // Placeholder image
+            type: "Mythic Weapon",
+            price: "50 Mythic Prisms (Base)",
+            description: "Unleash a demonic fury with Reinhardt's customizable weapon."
+        }
+    ]
+};
